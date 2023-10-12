8cefc76d9aa9d459546dfbbb76cfdd025299d878
Fumiya-Kito <cfbc.pearl@icloud.com>
2023-10-12 22:18:27 +0900

    feat: 231012

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>TIL by Fumiya Kito</span>,
+  logo: <span>TIL by Kito</span>,
   project: {
     link: 'https://github.com/Fumiya-Kito',
   },
